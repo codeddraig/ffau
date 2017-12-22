9c0c114564b12309a836b75aaf23a9b1bd78a27c
gezakerecsenyi <32415108+gezakerecsenyi@users.noreply.github.com>
2017-12-22 18:04:10 +0000

    Update en2.js

--- en2.js
+++ en2.js
@@ -79,9 +79,7 @@ Msg = {
       "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
     },
     "headline": {
-      //Theodore, how would we make the closing tag here? I am not proficient in Javascript, so see if you can figure something out.
-      
-      var infoOne = "%1"
+      //Theodore, how would we make the closing tag here? I am not fully proficient in Javascript, so see if you can figure something out.
       "message0": "<h %1 > %2 %3 </h1>",
       "tooltip": "",
       "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
