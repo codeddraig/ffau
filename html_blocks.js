3f04b1ac8b385abe22c7edb6bdde13bc26c9f5a8
gezakerecsenyi <32415108+gezakerecsenyi@users.noreply.github.com>
2017-12-23 13:11:44 +0000

    Update html_blocks.js

--- html_blocks.js
+++ html_blocks.js
@@ -402,7 +402,7 @@ var htmlBlocks =
 },
 {
   "type": "headline",
-  "message0": "headline %1 %2 %3 %4",
+  "message0": "headline %1 %2 %3",
   "args0": [
   {
     "type": "field_dropdown",
@@ -440,36 +440,6 @@ var htmlBlocks =
   {
     "type": "input_statement",
     "name": "content"
-  },
-  {
-    "type": "field_dropdown",
-    "name": "NAME",
-    "options": [
-    [
-    "level 1",
-    "h1"
-    ],
-    [
-    "level 2",
-    "h2"
-    ],
-    [
-    "level 3",
-    "h2"
-    ],
-    [
-    "level 4",
-    "h4"
-    ],
-    [
-    "level 5",
-    "h5"
-    ],
-    [
-    "level 6",
-    "h6"
-    ]
-    ]
   },
   ],
   "previousStatement": null,
