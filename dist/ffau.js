d23041185dda9f921d76181438af20df9c0f6feb
Geza Kerecsenyi <gezakerecsenyi@gmail.com>
2019-10-24 13:24:56 +0100

    Add reverse coding of CSS color names.

--- dist/ffau.js
+++ dist/ffau.js
@@ -37,6 +37,9 @@ function fontAwesome(icon) {
     Blockly.HSV_VALUE = 0.7;
 }());
 
+const colorNames = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'].map(e => e.toLowerCase());
+const colorValues = ['f0f8ff', 'faebd7', '00ffff', '7fffd4', 'f0ffff', 'f5f5dc', 'ffe4c4', '000000', 'ffebcd', '0000ff', '8a2be2', 'a52a2a', 'deb887', '5f9ea0', '7fff00', 'd2691e', 'ff7f50', '6495ed', 'fff8dc', 'dc143c', '00ffff', '00008b', '008b8b', 'b8860b', 'a9a9a9', 'a9a9a9', '006400', 'bdb76b', '8b008b', '556b2f', 'ff8c00', '9932cc', '8b0000', 'e9967a', '8fbc8f', '483d8b', '2f4f4f', '2f4f4f', '00ced1', '9400d3', 'ff1493', '00bfff', '696969', '696969', '1e90ff', 'b22222', 'fffaf0', '228b22', 'ff00ff', 'dcdcdc', 'f8f8ff', 'ffd700', 'daa520', '808080', '808080', '008000', 'adff2f', 'f0fff0', 'ff69b4', 'cd5c5c', '4b0082', 'fffff0', 'f0e68c', 'e6e6fa', 'fff0f5', '7cfc00', 'fffacd', 'add8e6', 'f08080', 'e0ffff', 'fafad2', 'd3d3d3', 'd3d3d3', '90ee90', 'ffb6c1', 'ffa07a', '20b2aa', '87cefa', '778899', '778899', 'b0c4de', 'ffffe0', '00ff00', '32cd32', 'faf0e6', 'ff00ff', '800000', '66cdaa', '0000cd', 'ba55d3', '9370db', '3cb371', '7b68ee', '00fa9a', '48d1cc', 'c71585', '191970', 'f5fffa', 'ffe4e1', 'ffe4b5', 'ffdead', '000080', 'fdf5e6', '808000', '6b8e23', 'ffa500', 'ff4500', 'da70d6', 'eee8aa', '98fb98', 'afeeee', 'db7093', 'ffefd5', 'ffdab9', 'cd853f', 'ffc0cb', 'dda0dd', 'b0e0e6', '800080', '663399', 'ff0000', 'bc8f8f', '4169e1', '8b4513', 'fa8072', 'f4a460', '2e8b57', 'fff5ee', 'a0522d', 'c0c0c0', '87ceeb', '6a5acd', '708090', '708090', 'fffafa', '00ff7f', '4682b4', 'd2b48c', '008080', 'd8bfd8', 'ff6347', '40e0d0', 'ee82ee', 'f5deb3', 'ffffff', 'f5f5f5', 'ffff00', '9acd32'];
+
 /**
  * @class Class representing a Ffau instance, including all components.
  */
@@ -614,7 +617,7 @@ class Ffau {
         function extractStyleSheet(styleSheetString, hasSelectors) {
             function extractStyles(styleString) {
                 let uniqueId = (Math.floor(Math.random() * 900000) + 100000).toString();
-                while (styleString.indexOf(uniqueId) > -1)
+                while (styleString.includes(uniqueId))
                     uniqueId = (Math.floor(Math.random() * 900000) + 100000).toString();
 
                 let stringSplit = (" " + styleString)
@@ -644,7 +647,7 @@ class Ffau {
                 return extractStyles(styleSheetString);
             else {
                 let uniqueId = (Math.floor(Math.random() * 9000000) + 1000000).toString();
-                while (styleSheetString.indexOf(uniqueId) > -1)
+                while (styleSheetString.includes(uniqueId))
                     uniqueId = (Math.floor(Math.random() * 9000000) + 1000000).toString();
 
                 let selectors = (" " + styleSheetString)
@@ -670,7 +673,7 @@ class Ffau {
         function constructStyleTree(selectors, initialParent) {
             let parent = initialParent;
             selectors.forEach((selector, m) => {
-                function colorBlock() {
+                function mapColorLikeBlock() {
                     let colorValue = document.createElement("value");
                     colorValue.setAttribute("name", "value");
 
@@ -680,6 +683,7 @@ class Ffau {
                     if (/#(?:[0-9a-fA-F]{3}){1,2}/g.test(selector[1].trim())) {
                         valueField.parentNode.removeChild(valueField);
                         valueField.setAttribute("name", "color");
+                        valueField.innerText = selector[1].trim().replace(/^#/g, "");
 
                         colorBlock.setAttribute("type", "hex_picker");
 
@@ -688,6 +692,7 @@ class Ffau {
                     } else if (/rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)/g.test(selector[1].trim())) {
                         valueField.parentNode.removeChild(valueField);
                         valueField.setAttribute("name", "a");
+                        valueField.innerText = selector[1].trim().replace(/^#/g, "");
 
                         colorBlock.setAttribute("type", "rgba_picker");
 
@@ -714,9 +719,23 @@ class Ffau {
                         }
 
                         colorValue.appendChild(colorBlock);
-                    } /*else {
-                        if (Blockly.selector[1].trim())
-                    }*/
+                    } else {
+                        if (Blockly.FieldColour.TITLES.includes(selector[1].trim())) {
+                            valueField.parentNode.removeChild(valueField);
+                            colorBlock.setAttribute("type", "color_picker");
+
+                            valueField.setAttribute("name", "color");
+                            valueField.innerHTML = Blockly.FieldColour.COLOURS[
+                                Blockly.FieldColour.TITLES.indexOf(selector[1].trim())
+                                ];
+
+                            colorBlock.appendChild(valueField);
+                            colorValue.appendChild(colorBlock);
+                        } else {
+                            selector[1] = "#" + colorValues[colorNames.indexOf(selector[1].toLowerCase())];
+                            mapColorLikeBlock();
+                        }
+                    }
 
                     styleBlock.appendChild(colorValue);
                 }
@@ -740,7 +759,7 @@ class Ffau {
                         break;
                     case 'color':
                         styleBlock.setAttribute("type", "color-new");
-                        colorBlock();
+                        mapColorLikeBlock();
                         break;
                     default:
                         styleBlock.setAttribute("type", "othercss");
@@ -784,15 +803,15 @@ class Ffau {
         parallelTree.setAttribute("xmlns", "http://www.w3.org/1999/xhtml");
 
         let tempId = (Math.floor(Math.random() * 90000) + 10000).toString();
-        while (code.indexOf(tempId) > -1)
+        while (code.includes(tempId))
             tempId = (Math.floor(Math.random() * 90000) + 10000).toString();
 
         let tagId = (Math.floor(Math.random() * 90000) + 10000).toString();
-        while (code.indexOf(tagId) > -1 || tagId === tempId)
+        while (code.includes(tagId) || tagId === tempId)
             tagId = (Math.floor(Math.random() * 90000) + 10000).toString();
 
         let nonStringId = (Math.floor(Math.random() * 90000) + 10000).toString();
-        while (code.indexOf(nonStringId) > -1 || nonStringId === tempId || nonStringId === tagId)
+        while (code.includes(nonStringId) || nonStringId === tempId || nonStringId === tagId)
             nonStringId = (Math.floor(Math.random() * 90000) + 10000).toString();
 
         let bodifiedCode = (" " + code)
